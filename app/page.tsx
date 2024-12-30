d866775cd66ded9f076224d4373809a506164d3a
yasir-panther <yasir@pantherprotocol.io>
2024-12-30 14:24:58 +0530

    feat(setup): project init

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@ export default function Home() {
   return (
     <main>
       <h2>DSA Essentials</h2>
-      <p>A personal learning hub for frontend engineers tackling the basics of DSA. Straightforward, focused, and practical.</p>
+      <p>
+        A personal learning hub for frontend engineers tackling the basics of
+        DSA. Straightforward, focused, and practcal.
+      </p>
     </main>
   );
 }
