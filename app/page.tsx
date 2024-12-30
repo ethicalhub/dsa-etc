3b36e6e7a5e0ba271789f05289087906bffa908c
yasir-panther <yasir@pantherprotocol.io>
2024-12-30 14:18:46 +0530

    add proj

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h2>DSA Essentials</h2>
+      <p>A personal learning hub for frontend engineers tackling the basics of DSA. Straightforward, focused, and practical.</p>
+    </main>
+  );
+}
