5a1a89c3a829080f9818e5ef64c0fb85a51bb47f
yasir-panther <yasir@pantherprotocol.io>
2025-02-16 06:10:19 +0530

    update count Digit

--- Mathematics/countDigit.js
+++ Mathematics/countDigit.js
@@ -1,13 +1,30 @@
-const countDigit = (n) => {
-    if (n === 0) return 1;  // ✅ Edge case for 0
+console.log("------ Count the number of digits of a given number without converting them into string or array")
 
-    let count = 0;
-    n = Math.abs(n);
 
-    while (n > 0) {
-        n = Math.floor(n / 10);
-        count++;    
+let a = 0
+let b = 2346030
+let c = -2354
+
+
+const countDigit = (x)=>{
+    let digitCount = 0;
+    if(x===0) return 1;
+
+    // To handle negative integers but it can also be handles with Math.abs()
+    // if(x<0) {
+    //     x = -x;
+    // }
+
+    x = Math.abs(x)
+    
+    while(x>=1){
+        x = Math.floor(x/10);  
+        digitCount++;
     }
-    console.log(count);
-    return count;
-};
+    return digitCount;
+}
+
+
+console.log(countDigit(a))
+console.log(countDigit(b))
+console.log(countDigit(c))
